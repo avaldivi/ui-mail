05fe3dd63f7c4e51705f27a67b30cb237cd1c7f0
Adrianna Valdivia <avaldivi@ggc.edu>
2018-12-02 19:14:13 -0500

    Update file structure.

---
+++ precache-manifest.97e2d85e27f2d4332da2f8011165a41f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d5dc675de9495a95361",
+    "url": "/ui-mail/static/css/main.f9c7d904.chunk.css"
+  },
+  {
+    "revision": "9d5dc675de9495a95361",
+    "url": "/ui-mail/static/js/main.9d5dc675.chunk.js"
+  },
+  {
+    "revision": "9fe10223bc049419feb6",
+    "url": "/ui-mail/static/js/1.9fe10223.chunk.js"
+  },
+  {
+    "revision": "21e3145177809a66f91c",
+    "url": "/ui-mail/static/js/runtime~main.21e31451.js"
+  },
+  {
+    "revision": "f070cd69fc02472014237dd098bc8a97",
+    "url": "/ui-mail/index.html"
+  }
+];
